1b0f4573dccf1c199a413aea9b50e18b7a2714e1
waveay <dominik.rene.danner@gmail.com>
2021-05-14 14:56:19 +0200

    #15 Documentaion Fixes | More Configs

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,23 @@
+import color from 'colors'
 import { Client } from 'discord.js'
 import { bot } from './config/config'
 import { Clear } from './command/command'
-import { DynamicChannel } from './voice/voice'
+import { TempChannel } from './voice/temp-channel'
 import { MusicPlayer } from './voice/audio-handler'
+import { DatabaseLogger } from './database/database'
 const client = new Client()
 const audio = new MusicPlayer(client)
 
-client.on('ready', () => {
-    console.log(`Logged in as : ${ client.user?.tag }`)
-    client.user?.setPresence({
-        status: 'online',
-        activity: {
-            type: 'LISTENING',
-            name: 'den Schülern'
-        }
-    })
+client.on('ready', async() => {
+    console.log(`--------- ${color.grey('General Info')} ---------`)
+    console.log(`Login:     ${ color.green(client.user!.tag) }`)
+    await DatabaseLogger();
+    client.user?.setPresence(bot.presence)
     
     //Clear Command
     Clear(client, bot.commands.clear.name)
     //Creates Dynamic or Temp Channel
-    DynamicChannel(client)
+    TempChannel(client)
     //Audio-Handler for the music
     audio.MusicPlayer()
 })
